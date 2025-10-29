7f0671196cb48f800870ab67c890dc357081eaeb
dicenull <dicenull.dev@gmail.com>
2025-10-29 12:42:31 +0900

    敵を倒せるようにした

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
